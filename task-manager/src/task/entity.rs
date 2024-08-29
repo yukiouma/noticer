8d1c97f1c1d23227e6d797b169002d6d59bc0ebe
Yuki Chan <2624466175@qq.com>
2024-08-29 22:26:01 +0800

    fix: init dao and entity

--- task-manager/src/task/entity.rs
+++ task-manager/src/task/entity.rs
@@ -1,22 +1,23 @@
 use chrono::{DateTime, Datelike, Local, Timelike};
+use sqlx::FromRow;
 
-const ONE_DAY_MINUTE: u32 = 1440;
+const ONE_DAY_MINUTE: i32 = 1440;
 
 #[derive(Debug, Default)]
 pub struct Task {
-    id: u64,
+    id: i32,
     // task basic info
     name: String,
     description: String,
 
     // task schedule parameters
     expect_times: Option<i32>,
-    month: Option<u16>,
-    day: Option<u32>,
-    weekday: Option<u8>,
-    timepoint: Option<u32>,
-    time_gap: Option<u32>,
-    duration: Option<(u32, u32)>,
+    month: Option<i32>,
+    day: Option<i32>,
+    weekday: Option<i32>,
+    timepoint: Option<i32>,
+    time_gap: Option<i32>,
+    duration: Option<(i32, i32)>,
     execute_times: i32,
     last_execute_at: Option<DateTime<Local>>,
 }
@@ -42,7 +43,7 @@ impl Task {
     pub fn description(&self) -> &str {
         &self.description
     }
-    pub fn set_month(&mut self, month: u32) -> &mut Self {
+    pub fn set_month(&mut self, month: i32) -> &mut Self {
         let mut m = match self.month {
             Some(m) => m,
             None => 0,
@@ -53,7 +54,7 @@ impl Task {
         }
         self
     }
-    pub fn month(&self) -> Option<Vec<u32>> {
+    pub fn month(&self) -> Option<Vec<i32>> {
         let month = self.month?;
         let mut months = vec![];
         for i in 0..12 {
@@ -63,13 +64,13 @@ impl Task {
         }
         Some(months)
     }
-    pub fn match_month(&self, month: u32) -> bool {
+    pub fn match_month(&self, month: i32) -> bool {
         match self.month {
             Some(m) => (m | !(1 << month - 1)).eq(&!0),
             None => true,
         }
     }
-    pub fn set_weekday(&mut self, weekday: u32) -> &mut Self {
+    pub fn set_weekday(&mut self, weekday: i32) -> &mut Self {
         let mut w = match self.weekday {
             Some(w) => w,
             None => 0,
@@ -80,7 +81,7 @@ impl Task {
         }
         self
     }
-    pub fn weekday(&self) -> Option<Vec<u32>> {
+    pub fn weekday(&self) -> Option<Vec<i32>> {
         let weekday = self.weekday?;
         let mut weekdays = vec![];
         for i in 0..7 {
@@ -90,13 +91,13 @@ impl Task {
         }
         Some(weekdays)
     }
-    pub fn match_weekday(&self, weekday: u32) -> bool {
+    pub fn match_weekday(&self, weekday: i32) -> bool {
         match self.weekday {
             Some(w) => (w | !(1 << weekday - 1)).eq(&!0),
             None => true,
         }
     }
-    pub fn set_day(&mut self, day: u32) -> &mut Self {
+    pub fn set_day(&mut self, day: i32) -> &mut Self {
         let mut d = match self.day {
             Some(d) => d,
             None => 0,
@@ -107,7 +108,7 @@ impl Task {
         }
         self
     }
-    pub fn day(&self) -> Option<Vec<u32>> {
+    pub fn day(&self) -> Option<Vec<i32>> {
         let day = self.day?;
         let mut days = vec![];
         for i in 0..31 {
@@ -117,31 +118,31 @@ impl Task {
         }
         Some(days)
     }
-    pub fn match_day(&self, day: u32) -> bool {
+    pub fn match_day(&self, day: i32) -> bool {
         match self.day {
             Some(d) => (d | !(1 << day - 1)).eq(&!0),
             None => true,
         }
     }
-    pub fn set_timepoint(&mut self, hour: u32, minute: u32) -> &mut Self {
+    pub fn set_timepoint(&mut self, hour: i32, minute: i32) -> &mut Self {
         self.timepoint = Some(hour * 60 + minute);
         self
     }
-    pub fn timepoint(&self) -> Option<(u32, u32)> {
+    pub fn timepoint(&self) -> Option<(i32, i32)> {
         let timepoint = self.timepoint?;
         let hour = timepoint / 60;
         let minute = timepoint % 60;
         Some((hour, minute))
     }
 
-    pub fn match_timepoint(&self, hour: u32, minute: u32) -> bool {
+    pub fn match_timepoint(&self, hour: i32, minute: i32) -> bool {
         match self.timepoint {
             Some(t) => t == hour * 60 + minute,
             None => true,
         }
     }
 
-    pub fn set_time_gap(&mut self, time_gap: u32) -> &mut Self {
+    pub fn set_time_gap(&mut self, time_gap: i32) -> &mut Self {
         if time_gap.le(&ONE_DAY_MINUTE) {
             self.time_gap = Some(time_gap);
         }
@@ -152,7 +153,7 @@ impl Task {
         match self.time_gap {
             Some(gap) => {
                 if let Some(last_execute_at) = self.last_execute_at {
-                    let since = now.signed_duration_since(last_execute_at).num_minutes() as u32;
+                    let since = now.signed_duration_since(last_execute_at).num_minutes() as i32;
                     since.ge(&gap)
                 } else {
                     true
@@ -162,7 +163,7 @@ impl Task {
         }
     }
 
-    pub fn set_duration(&mut self, start: (u32, u32), end: (u32, u32)) -> &mut Self {
+    pub fn set_duration(&mut self, start: (i32, i32), end: (i32, i32)) -> &mut Self {
         let start = start.0 * 60 + start.1;
         let end = end.0 * 60 + end.1;
         if start.gt(&ONE_DAY_MINUTE) || end.gt(&ONE_DAY_MINUTE) || start.gt(&end) {
@@ -172,7 +173,7 @@ impl Task {
         self
     }
 
-    pub fn duration(&self) -> Option<((u32, u32), (u32, u32))> {
+    pub fn duration(&self) -> Option<((i32, i32), (i32, i32))> {
         let duration = self.duration?;
         let duration_start = duration.0;
         let duration_end = duration.1;
@@ -186,7 +187,7 @@ impl Task {
         }
     }
 
-    pub fn match_duration(&self, hour: u32, minute: u32) -> bool {
+    pub fn match_duration(&self, hour: i32, minute: i32) -> bool {
         match self.duration {
             Some(duration) => {
                 let timepoint = hour * 60 + minute;
@@ -202,6 +203,10 @@ impl Task {
         self
     }
 
+    pub fn expect_times(&self) -> Option<i32> {
+        self.expect_times
+    }
+
     pub fn less_expect_times(&self) -> bool {
         match self.expect_times {
             Some(expect_times) => expect_times.gt(&self.execute_times),
@@ -212,11 +217,11 @@ impl Task {
     pub fn ready_to_execute(&self) -> bool {
         // get current time
         let now = Local::now();
-        let month = now.month();
-        let day = now.day();
+        let month = now.month().try_into().unwrap();
+        let day = now.day().try_into().unwrap();
         let weekday = now.weekday();
-        let hour = now.hour();
-        let minute = now.minute();
+        let hour = now.hour().try_into().unwrap();
+        let minute = now.minute().try_into().unwrap();
         let gap = if let Some(last_execute_at) = self.last_execute_at {
             Some(now.signed_duration_since(&last_execute_at))
         } else {
@@ -225,7 +230,7 @@ impl Task {
 
         if self.match_month(month)
             && self.match_day(day)
-            && self.match_weekday(weekday.num_days_from_monday() + 1)
+            && self.match_weekday((weekday.num_days_from_monday() + 1).try_into().unwrap())
             && self.match_duration(hour, minute)
             && self.match_timepoint(hour, minute)
             && self.less_expect_times()
@@ -236,16 +241,91 @@ impl Task {
             false
         }
     }
+
     pub fn execute(&mut self) -> &mut Self {
         self.last_execute_at = Some(Local::now());
         self.execute_times += 1;
         self
     }
+
     pub fn execute_times(&self) -> i32 {
         self.execute_times
     }
+
     pub fn last_execute_at(&self) -> Option<DateTime<Local>> {
         self.last_execute_at.clone()
+    }
+}
+
+#[derive(Debug, FromRow)]
+pub struct TaskDAO {
+    pub id: i32,
+    pub name: String,
+    pub description: String,
+    pub expect_times: Option<i32>,
+    pub month: Option<i32>,
+    pub day: Option<i32>,
+    pub weekday: Option<i32>,
+    pub timepoint: Option<i32>,
+    pub time_gap: Option<i32>,
+    pub duration_start: Option<i32>,
+    pub duration_end: Option<i32>,
+    pub execute_times: i32,
+    pub last_execute_at: Option<DateTime<Local>>,
+}
+
+impl From<TaskDAO> for Task {
+    fn from(value: TaskDAO) -> Self {
+        Task {
+            id: value.id,
+            name: value.name,
+            description: value.description,
+            expect_times: value.expect_times,
+            month: value.month,
+            day: value.day,
+            weekday: value.weekday,
+            timepoint: value.weekday,
+            time_gap: value.time_gap,
+            duration: if let Some(start) = value.duration_start {
+                if let Some(end) = value.duration_end {
+                    Some((start, end))
+                } else {
+                    None
+                }
+            } else {
+                None
+            },
+            execute_times: value.execute_times,
+            last_execute_at: value.last_execute_at,
+        }
+    }
+}
+
+impl Into<TaskDAO> for Task {
+    fn into(self) -> TaskDAO {
+        TaskDAO {
+            id: self.id,
+            name: self.name.clone(),
+            description: self.description.clone(),
+            expect_times: self.expect_times,
+            month: self.month,
+            day: self.day,
+            weekday: self.weekday,
+            timepoint: self.timepoint,
+            time_gap: self.time_gap,
+            duration_start: if let Some(duration) = self.duration {
+                Some(duration.0)
+            } else {
+                None
+            },
+            duration_end: if let Some(duration) = self.duration {
+                Some(duration.1)
+            } else {
+                None
+            },
+            execute_times: self.execute_times,
+            last_execute_at: self.last_execute_at,
+        }
     }
 }
 
--- task-manager/src/task/repo.rs
+++ task-manager/src/task/repo.rs
@@ -1,14 +1,35 @@
 use sqlx::MySqlPool;
 
+use super::entity::{Task, TaskDAO};
+
 pub struct TaskRepo {
     pool: MySqlPool,
 }
 
 impl TaskRepo {
-    pub async fn new(pool: MySqlPool) -> TaskRepo {
+    pub  fn new(pool: MySqlPool) -> TaskRepo {
         TaskRepo { pool }
     }
-    pub async fn get_task_by_name(&self, task: &str) -> anyhow::Result<()> {
+    pub async fn list_tasks(&self) -> anyhow::Result<Vec<Task>> {
+        Ok(sqlx::query_as::<_, TaskDAO>("SELECT * FROM `task`").fetch_all(&self.pool).await?.into_iter().map(|dao|dao.into()).collect::<Vec<Task>>())
+    }
+    pub async fn create_task(&self) -> anyhow::Result<()> {
+        // sqlx::query!("INSERT INTO `task` () VALUE ()").execute(&self.pool).await?
+        Ok(())
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[tokio::test]
+    async fn repo_test() -> anyhow::Result<()> {
+        let database_url = "mysql://root:000000@localhost:3306/noticer?parseTime=True";
+        let pool = MySqlPool::connect(&database_url).await?;
+        let repo = TaskRepo::new(pool);
+        let tasks = repo.list_tasks().await?;
+        assert_eq!(tasks.len(), 0);
         Ok(())
     }
 }
