e01ec8d058f833eedf98419a4d25ba84a14009ed
yuki <2624466175@qq.com>
2024-09-10 04:34:52 +0000

    feat: schedueler run

--- crates/task-manager/src/main.rs
+++ crates/task-manager/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+
+use sqlx::MySqlPool;
+use task_manager::{Scheduler, TaskRepo};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let database_url = "mysql://root:000000@localhost:3306/noticer?parseTime=True";
+    let pool = MySqlPool::connect(&database_url).await?;
+    let repo = Arc::new(TaskRepo::new(pool));
+
+    let scheduler = Scheduler::new(repo.clone());
+    let tx = scheduler.run()?;
+    loop {
+        let task_id = tx.recv().unwrap();
+        let mut task = repo.find_task_by_id(task_id).await.unwrap().unwrap();
+        task.execute();
+        repo.update_task(&task).await.unwrap();
+        println!("task id: {}", task_id);
+    }
+    // Ok(())
 }
--- crates/task-manager/src/scheduler/scheduler.rs
+++ crates/task-manager/src/scheduler/scheduler.rs
@@ -1,42 +1,42 @@
 use std::{
-    sync::mpsc::{self, Receiver},
+    sync::{
+        mpsc::{self, Receiver},
+        Arc,
+    },
     thread,
     time::Duration,
 };
 
 use crate::TaskRepo;
 
-pub struct Scheduler<'a> {
-    repo: &'a TaskRepo,
+pub struct Scheduler {
+    repo: Arc<TaskRepo>,
 }
 
-impl<'a> Scheduler<'a> {
-    pub fn new(repo: &'a TaskRepo) -> Scheduler {
+impl Scheduler {
+    pub fn new(repo: Arc<TaskRepo>) -> Scheduler {
         Scheduler { repo }
     }
-    async fn list_ready_task_id(&self) -> anyhow::Result<Vec<i32>> {
-        let tasks = self.repo.list_tasks().await?;
 
-        let ready_task_id = tasks
-            .into_iter()
-            .filter(|task| task.ready_to_execute())
-            .map(|task| task.id())
-            .collect::<Vec<_>>();
-        Ok(ready_task_id)
-    }
     pub fn run(&self) -> anyhow::Result<Receiver<i32>> {
         let (tx, rx) = mpsc::channel::<i32>();
-        let rt = tokio::runtime::Builder::new_current_thread()
-            .enable_all()
-            .build()?;
-        // thread::spawn(move || loop {
-        //     if let Ok(ready_task_id) = rt.block_on(self.list_ready_task_id()) {
-        //         // TODO: send task id to message queue
-        //         println!("Sending task id: {:?} to message queue", ready_task_id);
-        //         ready_task_id.iter().for_each(|id| tx.send(*id).unwrap());
-        //     };
-        //     thread::sleep(Duration::from_secs(10));
-        // });
+        let repo = self.repo.clone();
+        tokio::spawn(async move {
+            loop {
+                if let Ok(tasks) = repo.list_tasks().await {
+                    let ready_task_id = tasks
+                        .into_iter()
+                        .filter(|task| task.ready_to_execute())
+                        .map(|task| task.id())
+                        .collect::<Vec<_>>();
+                    println!("Ready task id: {:?}", ready_task_id);
+                    for id in ready_task_id {
+                        tx.send(id).unwrap();
+                    }
+                }
+                thread::sleep(Duration::from_secs(10));
+            }
+        });
         Ok(rx)
     }
 }
